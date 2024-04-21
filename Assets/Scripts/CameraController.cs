a2c55ad6109983cf8b7fd670c408178de9433cef
DanielCorso009 <dcorso1@student.fitchburgstate.edu>
2024-04-21 14:30:57 -0400

    tileset and tileset collisions added

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,30 +5,33 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    private float boundaryX = 14;
-    private float boundaryY =5f;
-    
+    private float boundaryX = 10;
+    private float boundaryYTop =7f;
+    private float boundaryYBottom =4;
+    //private Camera camera;
     private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        //camera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        boundaryX = 12;
         if(player.transform.position.x > transform.position.x + boundaryX){
             transform.position = new Vector3(transform.position.x + 2*boundaryX,transform.position.y,-10);
         }        
         else if(player.transform.position.x < transform.position.x - boundaryX){
             transform.position = new Vector3(transform.position.x - 2*boundaryX,transform.position.y,-10);
         }
-        if(player.transform.position.y > transform.position.y + boundaryY+2.5){
-            transform.position = new Vector3(transform.position.x,transform.position.y + 2*boundaryY+2.5f,-10);
+        if(player.transform.position.y > transform.position.y + boundaryYTop){
+            transform.position = new Vector3(transform.position.x,transform.position.y + 11,-10);
         }        
-        else if(player.transform.position.y < transform.position.y - boundaryY){
-            transform.position =  new Vector3(transform.position.x,transform.position.y - 2*boundaryY-2.5f,-10);
+        else if(player.transform.position.y < transform.position.y - boundaryYBottom){
+            transform.position =  new Vector3(transform.position.x,transform.position.y - 11,-10);
         }
     }
 }
